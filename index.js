50fb11b7a1f89a759d07fd1afa3be2b813f71b34
Amita <panwar.amita@gmail.com>
2021-02-04 21:33:20 -0500

    updated the index.js file

--- index.js
+++ index.js
@@ -20,44 +20,39 @@ function CreateQuestions() {
         {
             name: 'name',
             type: 'input',
-            message: `Enter the team member's name?`
+            message: `Enter the Manager's name?`
         },
         {
             name: 'id',
             type: 'input',
-            message: `Enter the team member's ID?`
+            message: `Enter the Managers's ID?`
         },
         {
             name: 'email',
             type: 'input',
-            message: `Enter the team member's email address?`,
+            message: `Enter the Managers's email address?`,
             validate: (email) => {
                 if (validateEmail.validate(email))
-                  return true;
+                    return true;
                 else
-                  return `${email} is not valid`
-              }
+                    return `${email} is not valid`
+            }
         },
         {
-            name: 'role',
-            type: 'list',
-            message: `What is the team member role?`,
-            choices: ['Manager', 'Engineer', 'Intern']
+            name: 'officeNumber',
+            type: 'input',
+            message: `Enter the office number?`,
+            validate: (officeNumber) => {
+                if (officeNumber.match(/^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g) && officeNumber.match(/\d/g).length === 10)
+                    return true;
+                else
+                    return `${officeNumber} is not valid`
+            }
+        },
 
-        }
     ]).then(answers => {
-        if (answers.role == "Engineer") {
-            //call the function to prompt engineer details
-            addEngineer(answers);
-        }
-        else if (answers.role == "Intern") {
-            //call the function to prompt intern details
-            addIntern(answers);
-        }
-        else if (answers.role == "Manager") {
-            //call the function to prompt intern details
-            addManager(answers);
-        }
+        addManager(answers);
+
         //function ask the user if wants to add more people or we want to quit
         // addMore();
     });
@@ -75,7 +70,24 @@ function addMore() {
     ]).then(answers => {
         if (answers.addMore == 'Yes') {
             //function to ask if wants to add more people
-            CreateQuestions();
+            inquirer.prompt([
+                {
+                    name: 'role',
+                    type: 'list',
+                    message: `What is the team member role?`,
+                    choices: ['Engineer', 'Intern']
+
+                }
+            ]).then(answers => {
+                if (answers.role == "Engineer") {
+                    //call the function to prompt engineer details
+                    addEngineer();
+                }
+                else if (answers.role == "Intern") {
+                    //call the function to prompt intern details
+                    addIntern();
+                }
+            });
         }
         else {
             console.log('exit');
@@ -100,84 +112,114 @@ function addMore() {
 }
 //function to ask about engineer detials
 function addEngineer(data) {
-    inquirer.prompt([
-        {
-            name: 'githubUserName',
-            type: 'input',
-            message: `Enter the GitHub User name?`
-        }
-    ]).then(answers => {
-        //function to ask if wants to add more people
-        const engineerDetail = new engineer(data.id, data.name, data.email, answers.githubUserName);
-        //adding the data to the file
-        const read = details(engineerDetail);
-        fs.appendFileSync('./src/file.html', read, (err) => {
-            if (err) {
-                console.log(err);
+        inquirer.prompt([
+            {
+                name: 'name',
+                type: 'input',
+                message: `Enter the Engineer's name?`
+            },
+            {
+                name: 'id',
+                type: 'input',
+                message: `Enter the Engineer's ID?`
+            },
+            {
+                name: 'email',
+                type: 'input',
+                message: `Enter the Engineer's email address?`,
+                validate: (email) => {
+                    if (validateEmail.validate(email))
+                        return true;
+                    else
+                        return `${email} is not valid`
+                }
+            },
+            {
+                name: 'githubUserName',
+                type: 'input',
+                message: `Enter the Engineer's GitHub User name?`
             }
-            else {
-                console.log("suceesfully appended in to the file")
-            }
-        })
-        addMore();
-    });
+        ]).then(answers => {
+            //function to ask if wants to add more people
+            const engineerDetail = new engineer(answers.id, answers.name, answers.email, answers.githubUserName);
+            //adding the data to the file
+            const read = details(engineerDetail);
+            fs.appendFileSync('./src/file.html', read, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("suceesfully appended in to the file")
+                }
+            });
+            addMore();
+        });
+          
 }
 //function to ask about inter detials
 function addIntern(data) {
-    inquirer.prompt([
-        {
-            name: 'school',
-            type: 'input',
-            message: `Enter the school name?`
-        }
-    ]).then(answers => {
-        //function to ask if wants to add more people
-        const internDetail = new intern(data.id, data.name, data.email, answers.school);
-        //adding the data to the file
-        const read = details(internDetail);
-        fs.appendFileSync('./src/file.html', read, (err) => {
-            if (err) {
-                console.log(err);
+        inquirer.prompt([
+            {
+                name: 'name',
+                type: 'input',
+                message: `Enter the Intern's name?`
+            },
+            {
+                name: 'id',
+                type: 'input',
+                message: `Enter the Intern's ID?`
+            },
+            {
+                name: 'email',
+                type: 'input',
+                message: `Enter the Intern's email address?`,
+                validate: (email) => {
+                    if (validateEmail.validate(email))
+                        return true;
+                    else
+                        return `${email} is not valid`
+                }
+            },
+            {
+                name: 'school',
+                type: 'input',
+                message: `Enter Intern's the school name?`
             }
-            else {
-                console.log("suceesfully appended in to the file")
-            }
-        })
-        //function to ask if wants to add more people
+        ]).then(answers => {
+            //function to ask if wants to add more people
+            const internDetail = new intern(answers.id, answers.name, answers.email, answers.school);
+            //adding the data to the file
+            const read = details(internDetail);
+            fs.appendFileSync('./src/file.html', read, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("suceesfully appended in to the file")
+                }
+            })
+            //function to ask if wants to add more people
 
-        addMore();
-    });
+            addMore();
+        });
 }
 //function to ask about manager detials
 function addManager(data) {
-    inquirer.prompt([
-        {
-            name: 'officeNumber',
-            type: 'input',
-            message: `Enter the office number?`,
-            validate: (officeNumber) => {
-                if (officeNumber.match(/^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g)&&officeNumber.match(/\d/g).length===10)
-                  return true;
-                else
-                  return `${officeNumber} is not valid`
-              }
+
+    const managerDetail = new manager(data.id, data.name, data.email, data.officeNumber);
+    //adding the data to the file
+    console.log(managerDetail);
+    const read = details(managerDetail);
+    fs.appendFileSync('./src/file.html', read, (err) => {
+        if (err) {
+            console.log(err);
         }
-    ]).then(answers => {
-        const managerDetail = new manager(data.id, data.name, data.email, answers.officeNumber);
-        //adding the data to the file
-        console.log(managerDetail);
-        const read = details(managerDetail);
-        fs.appendFileSync('./src/file.html', read, (err) => {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log("suceesfully appended in to the file")
-            }
-        });
-         //function to ask if wants to add more people
-         addMore();
+        else {
+            console.log("suceesfully appended in to the file")
+        }
     });
+    //function to ask if wants to add more people
+    addMore();
 }
 function initApp() {
     fs.unlinkSync('./src/file.html');
